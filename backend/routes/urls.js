9643b724659afa071d1cc558038805a7801083c5
Renzo Perez <renzin@gmail.com>
2022-03-06 15:49:52 -0500

    adding link

--- backend/routes/urls.js
+++ backend/routes/urls.js
@@ -21,7 +21,7 @@ router.get('/shorten', async (req, res) => {
 // User post the long url
 router.post('/shorten', async (req, res) => {
   const { longUrl } = req.body;
-  const baseUrl = "https://testingshort-9v9173kah-derpmagician.vercel.app/";
+  const baseUrl = "https://testingshort.vercel.app/";
 
 
   // Check base url
